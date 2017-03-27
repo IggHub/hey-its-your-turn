be7bd84e97f74168513f6d68fbd1b085a44c2cf5
Iggy <igoririanto@rocketmail.com>
2017-03-27 12:10:32 -0700

    add schedule controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,11 @@
 Rails.application.routes.draw do
-  get 'schedules/index'
-
-  get 'schedules/show'
-
-  get 'schedules/new'
-
-  get 'schedules/create'
-
-  get 'schedules/edit'
-
-  get 'schedules/update'
-
-  get 'schedules/destroy'
 
   root to: "welcome#index"
 
-  get 'welcome/about'
-  get 'welcome/index'
+  get 'about' => 'welcome#about'
 
   devise_for :users
   resources :users
-
+  resources :schedules
   resources :workers, only: [:new, :create]
 end
---
+++ spec/controllers/schedules_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe SchedulesController, type: :controller do
+  let(:schedule) {FactoryGirl.build(:schedule)}
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, id: :schedule
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, id: :schedule
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
