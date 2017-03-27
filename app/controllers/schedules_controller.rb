e1aa5de0c34a2e6ec2cdbc6f389209883be1a397
Iggy <igoririanto@rocketmail.com>
2017-03-27 16:02:47 -0700

    edit and update schedules crud

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -10,5 +10,21 @@ class SchedulesController < ApplicationController
   end
 
   def edit
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def update
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update(schedule_params)
+      redirect_to @schedule, notice: 'Schedule was succesfffully updated'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:title, :due_at)
   end
 end
--- spec/controllers/schedules_controller_spec.rb
+++ spec/controllers/schedules_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SchedulesController, type: :controller do
-  let(:schedule) {FactoryGirl.build(:schedule)}
+  let(:schedule) {FactoryGirl.create(:schedule)}
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -25,9 +25,38 @@ RSpec.describe SchedulesController, type: :controller do
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit, id: :schedule
+      get :edit, id: schedule.id
       expect(response).to have_http_status(:success)
     end
+
+    it "renders edit view" do
+      get :edit, id: schedule.id
+      expect(response).to render_template(:edit)
+    end
+
+    it "assigns schedules to be updated to @schedule" do
+      get :edit, id: schedule.id
+      schedule_instance = assigns(:schedule)
+      expect(schedule_instance.id).to eq schedule.id
+      expect(schedule_instance.title).to eq schedule.title
+      expect(schedule_instance.due_at).to eq schedule.due_at
+    end
+  end
+
+  describe "PUT #update" do
+    it "updates schedules with specified attributes" do
+      new_title = RandomData.random_sentence
+      new_due_at = RandomData.random_due_at
+
+      put :update, id: schedule.id, schedule: {title: new_title, due_at: new_due_at}
+
+      updated_schedule = assigns(:schedule)
+
+      expect(updated_schedule.id).to eq schedule.id
+      expect(updated_schedule.title).to eq new_title
+      expect(updated_schedule.due_at.to_s).to eq new_due_at.utc.to_s #to_s because time RSpec testing differs by a few milliseconds
+    end
+
   end
 
 end
